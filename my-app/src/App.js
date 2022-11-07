6ce62d129948b66490b2e3eb5e97ac88bccbb333
klianglexpand <104772834+klianglexpand@users.noreply.github.com>
2022-11-06 20:45:54 -0500

    created login page

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,23 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
+import { MDBCardBody, MDBCardTitle, MDBCardText, MDBBtn, MDBCard, MDBRow, MDBCol, MDBCardOverlay, MDBCardImage, MDBFooter, MDBInput} from 'mdb-react-ui-kit';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <img src={logo} className="App-logo" alt="logo" />
+        <MDBCard className='w-25'>
+          <MDBCardTitle >Sign In</MDBCardTitle>
+          <hr></hr>
+          <MDBRow alignment='center' className='mb-2'>
+            <MDBInput label='Username' id='form1' type='text'></MDBInput>
+          </MDBRow>
+          <MDBRow alignment='center' className='mb-4'>
+            <MDBInput label='Password' id='form1' type='text'></MDBInput>
+          </MDBRow>
+          <MDBBtn className='btn btn-primary mb-2'>Login</MDBBtn>
+          <MDBBtn className='btn btn-secondary'>Sign up</MDBBtn>
+          <MDBBtn className="btn btn-light mt-2">
+            Contact Us
+          </MDBBtn>
+        </MDBCard>
     </div>
   );
 }
